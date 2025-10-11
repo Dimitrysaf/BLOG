6e5f48512a8f27760157ef16b3bde51437bec2f0
demetresmeliates <demetresmeliates@gmail.com>
2025-10-11 13:39:57 +0000

    MAJOR CLEANUP! INCOMPLETE

--- src/router.js
+++ src/router.js
@@ -1,43 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './pages/Home.vue';
 import NotFound from './pages/NotFound.vue';
-import User from './pages/User.vue';
 import Post from './pages/Post.vue';
-import loadingService from './loading';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
-  },
-  {
-    path: '/u/:username',
-    name: 'User',
-    component: User,
-    props: route => ({ user: route.meta.userData }),
-    async beforeEnter(to, from, next) {
-      loadingService.show();
-      try {
-        const username = to.params.username;
-        const response = await fetch(`/api/user/${username}`);
-
-        if (response.status === 404) {
-          next({ name: 'NotFound' });
-        } else if (!response.ok) {
-          throw new Error('Failed to fetch user data');
-        } else {
-          const userData = await response.json();
-          to.meta.userData = userData;
-          next();
-        }
-      } catch (error) {
-        console.error(error);
-        next({ name: 'NotFound' });
-      } finally {
-        loadingService.hide();
-      }
-    },
   },
   {
     path: '/p/:slug',
