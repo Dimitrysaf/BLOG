c03e19f681c2e0e7775e03892a5e026162b7dd6a
demetresmeliates <demetresmeliates@gmail.com>
2025-10-13 18:43:19 +0000

    .

--- src/codex.js
+++ src/codex.js
@@ -11,13 +11,13 @@ import {
   CdxProgressBar,
   CdxProgressIndicator,
   CdxTextArea,
-  CdxTextInput
+  CdxTextInput 
 } from '@wikimedia/codex';
 
 // Αυτή η συνάρτηση "εγκαθιστά" τα components στην Vue εφαρμογή σου.
 export function installCodex( app ) {
   app.component( 'CdxButton', CdxButton );
-  app.component( 'CdxButtonGroup', CdxButtonGroup );
+  app.component( 'CdxButtonGroup', CdxButtonGroup ); 
   app.component( 'CdxCard', CdxCard );
   app.component( 'CdxField', CdxField );
   app.component( 'CdxIcon', CdxIcon );
@@ -25,5 +25,5 @@ export function installCodex( app ) {
   app.component( 'CdxProgressBar', CdxProgressBar );
   app.component( 'CdxProgressIndicator', CdxProgressIndicator );
   app.component( 'CdxTextArea', CdxTextArea );
-  app.component( 'CdxTextInput', CdxTextInput );
+  app.component( 'CdxTextInput', CdxTextInput ); 
 }
