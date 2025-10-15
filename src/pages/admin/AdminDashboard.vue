d2446e7d969fbd1fff331df08aa5cfca1b33464a
demetresmeliates <demetresmeliates@gmail.com>
2025-10-15 19:48:25 +0000

    .

--- src/pages/admin/AdminDashboard.vue
+++ src/pages/admin/AdminDashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <Container>
     <div class="admin-dashboard">
-      <h1>Πίνακας Ελέγχου</h1>
+      <h1>Πίνακας Ελέγχou</h1>
       <cdx-tabs>
         <cdx-tab
           v-for="(tab, index) in tabsData"
@@ -10,7 +10,10 @@
           :label="tab.label"
           :disabled="tab.disabled"
         >
-          <div v-if="tab.name === 'posts'" class="tab-content">
+          <div v-if="tab.name === 'links'" class="tab-content">
+            <Links />
+          </div>
+          <div v-else-if="tab.name === 'posts'" class="tab-content">
             <Articles />
           </div>
           <div v-else-if="tab.name === 'users'" class="tab-content">
@@ -18,10 +21,6 @@
           </div>
           <div v-else-if="tab.name === 'comments'" class="tab-content">
             <Comments />
-          </div>
-          <div v-else class="tab-content">
-            <h2>{{ tab.label }}</h2>
-            <p>Περιεχόμενο για τη σελίδα "{{ tab.label }}".</p>
           </div>
         </cdx-tab>
       </cdx-tabs>
@@ -36,11 +35,12 @@ import Container from '../../components/Container.vue';
 import Articles from './Articles.vue';
 import Users from './Users.vue';
 import Comments from './Comments.vue';
+import Links from './Links.vue';
 
 const tabsData = ref([
   {
-    name: 'dashboard',
-    label: 'Dashboard',
+    name: 'links',
+    label: 'Σύνδεσμοι',
   },
   {
     name: 'users',
---
+++ src/pages/admin/Links.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="display: flex; flex-wrap: wrap; gap: 1rem; margin: -20px;">
+    <CdxCard
+      :thumbnail="{ url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqwNwDUq_S0U6wDzS60c45kVK5zpxF-03wsQ&s' }"
+      force-thumbnail="true"
+      @click="openLink('https://vercel.com/dimitrysafs-projects/dimblog')"
+      style="cursor: pointer;"
+    >
+      <template #title>Vercel Project</template>
+    </CdxCard>
+    <CdxCard
+      :thumbnail="{ url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLt5RQx6V1W6XXJcczgwNbzbdGyfHNCYtSCQ&s' }"
+      force-thumbnail="true"
+      @click="openLink('https://supabase.com/dashboard/project/flamyyopwhxrukyjhsya/editor/17366')"
+      style="cursor: pointer;"
+    >
+      <template #title>Supabase Dashboard</template>
+    </CdxCard>
+    <CdxCard
+      :thumbnail="{ url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Codex_logo.svg/1200px-Codex_logo.svg.png' }"
+      force-thumbnail="true"
+      @click="openLink('https://doc.wikimedia.org/codex/main/')"
+      style="cursor: pointer;"
+    >
+      <template #title>Wikimedia Codex</template>
+    </CdxCard>
+  </div>
+</template>
+
+<script setup>
+import { CdxCard } from '@wikimedia/codex';
+
+const openLink = (url) => {
+  window.open(url, '_blank', 'noopener,noreferrer');
+};
+</script>
